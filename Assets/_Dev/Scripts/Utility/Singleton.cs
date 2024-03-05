93837e21ce656c086705c46222b7f01b699ac875
Batuhan <batuhank1997@hotmail.com>
2024-03-05 21:53:25 +0300

    Update Singleton.cs

--- Assets/_Dev/Scripts/Utility/Singleton.cs
+++ Assets/_Dev/Scripts/Utility/Singleton.cs
@@ -2,40 +2,43 @@ using UnityEngine;
 
 namespace _Dev.Scripts.Utility
 {
-    public class Singleton<T> : UnityEngine.MonoBehaviour where T : Component {
+    public class Singleton<T> : UnityEngine.MonoBehaviour where T : Component
+    {
         private static object m_Lock = new object();
         private static T m_Instance;
 
         /// <summary>
         /// Access singleton instance through this propriety.
         /// </summary>
-        public static T Instance {
-            get {
-                lock (m_Lock) {
-                    if (m_Instance == null) {
-                        // Search for existing instance.
-                        m_Instance = (T)FindObjectOfType(typeof(T));
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    // Search for existing instance.
+                    m_Instance = (T)FindObjectOfType(typeof(T));
 
-                        // Create new instance if one doesn't already exist.
-                        if (m_Instance == null) {
-                            // Need to create a new GameObject to attach the singleton to.
-                            var singletonObject = new GameObject();
-                            m_Instance = singletonObject.AddComponent<T>();
-                            singletonObject.name = typeof(T).ToString() + " (Singleton)";
-                        }
+                    // Create new instance if one doesn't already exist.
+                    if (m_Instance == null)
+                    {
+                        // Need to create a new GameObject to attach the singleton to.
+                        var singletonObject = new GameObject();
+                        m_Instance = singletonObject.AddComponent<T>();
+                        singletonObject.name = typeof(T).ToString() + " (Singleton)";
                     }
-
-                    return m_Instance;
                 }
+
+                return m_Instance;
             }
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        protected static void Init() {
+        protected static void Init()
+        {
             m_Instance = null;
         }
 
         public static void SetInstance(T instance) => m_Instance = instance;
-
     }
 }
