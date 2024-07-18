b5231874ac0a7395db03dd9b8a14a7cf21dbcc97
Batuhan Köroğlu <51181106+batuhank1997@users.noreply.github.com>
2024-07-18 23:36:46 +0300

    change: item fall lerp

--- Assets/_Dev/Scripts/Data/ItemDistance.cs
+++ Assets/_Dev/Scripts/Data/ItemDistance.cs
@@ -1,4 +1,5 @@
-﻿using _Dev.Interfaces;
+﻿using System;
+using _Dev.Interfaces;
 using _Dev.Scripts.Managers;
 using UnityEngine;
 
@@ -7,37 +8,63 @@ namespace _Dev.Scripts.Data
     public class ItemDistance : ITickOnUpdate
     {
         public float Value;
-        
+        private float _timeElapsed;
+        private bool _isDirty;
+
         public ItemDistance(float value)
         {
             Value = value;
+            _timeElapsed = 0f;
+            _isDirty = false;
             UpdateHandler.Instance.Register(this);
         }
         
         public void Tick(float deltaTime)
         {
-            Decrease(deltaTime * 10f);
+            Decrease(LerpDistance(deltaTime));
+            _timeElapsed += deltaTime;
         }
         
         public void Increase(float value)
         {
+            _isDirty = true;
+            _timeElapsed = 0f;
             Value += value;
+        }
+        
+        public void Decrease(float lerp)
+        {
+            Value = Mathf.Lerp(Value, 0.5f, lerp);
         }
         
         public void Set(float value)
         {
+            _isDirty = true;
+            _timeElapsed = 0f;
             Value = value;
         }
                 
         public void Reset()
         {
+            _isDirty = true;
+            _timeElapsed = 0f;
             Value = InGameConstants.Item.StartingItemDistance;
         }
         
-        private void Decrease(float lerp)
+        private float LerpDistance(float t)
         {
-            Value = Mathf.Lerp(Value, 0.5f, lerp);
+            if (!_isDirty) return Value;
+            
+            if (_timeElapsed >= InGameConstants.Item.ItemDistanceLerpDuration)
+            {
+                _isDirty = false;
+                _timeElapsed = 0f;
+                return Value;
+            }
+            
+            t = _timeElapsed / InGameConstants.Item.ItemDistanceLerpDuration;
+            t = t * t * (3f - 2f * t);
+            return t;
         }
-
     }
 }
