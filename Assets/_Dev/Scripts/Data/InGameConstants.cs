ff5bda1548e7af3b9db17700a488e9e5211c157a
Batuhan Köroğlu <51181106+batuhank1997@users.noreply.github.com>
2024-07-20 01:42:49 +0300

    change: item fall by speed

--- Assets/_Dev/Scripts/Data/InGameConstants.cs
+++ Assets/_Dev/Scripts/Data/InGameConstants.cs
@@ -6,7 +6,8 @@
         {
             public const float StartingItemDistance = 0.5f;
             public const float ItemDistanceLerpDuration = 2f;
-            public const float NewItemCreationDelay = 0.2f;
+            public const float NewItemCreationDelay = 0f;
+            public const float ItemFallingSpeedMultiplier = 10f;
         }
     }
 }
--- Assets/_Dev/Scripts/Data/ItemDistance.cs
+++ Assets/_Dev/Scripts/Data/ItemDistance.cs
@@ -1,70 +1,48 @@
-﻿using System;
-using _Dev.Interfaces;
+﻿using _Dev.Interfaces;
 using _Dev.Scripts.Managers;
-using UnityEngine;
 
 namespace _Dev.Scripts.Data
 {
     public class ItemDistance : ITickOnUpdate
     {
         public float Value;
-        private float _timeElapsed;
-        private bool _isDirty;
 
         public ItemDistance(float value)
         {
             Value = value;
-            _timeElapsed = 0f;
-            _isDirty = false;
             UpdateHandler.Instance.Register(this);
         }
         
         public void Tick(float deltaTime)
         {
-            Decrease(LerpDistance(deltaTime));
-            _timeElapsed += deltaTime;
+            Decrease(deltaTime);
         }
         
         public void Increase(float value)
         {
-            _isDirty = true;
-            _timeElapsed = 0f;
             Value += value;
         }
         
-        public void Decrease(float lerp)
+        private void Decrease(float deltaTime)
         {
-            Value = Mathf.Lerp(Value, 0.5f, lerp);
+            if (Value > 0.5f)
+            {
+                Value -= InGameConstants.Item.ItemFallingSpeedMultiplier * deltaTime;
+                return;
+            }
+
+            if (Value <= 0.5f)
+                Value = 0.5f;
         }
         
         public void Set(float value)
         {
-            _isDirty = true;
-            _timeElapsed = 0f;
             Value = value;
         }
                 
         public void Reset()
         {
-            _isDirty = true;
-            _timeElapsed = 0f;
             Value = InGameConstants.Item.StartingItemDistance;
-        }
-        
-        private float LerpDistance(float t)
-        {
-            if (!_isDirty) return Value;
-            
-            if (_timeElapsed >= InGameConstants.Item.ItemDistanceLerpDuration)
-            {
-                _isDirty = false;
-                _timeElapsed = 0f;
-                return Value;
-            }
-            
-            t = _timeElapsed / InGameConstants.Item.ItemDistanceLerpDuration;
-            t = t * t * (3f - 2f * t);
-            return t;
         }
     }
 }
